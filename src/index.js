e6ef430b9827f9936091c56dcd182636b26e96a5
Ramy-Mansy <rami_manse@outlook.com>
2022-03-30 03:12:05 +0200

    remove dark mode

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import { ThemeProvider } from "./context";
 
 ReactDOM.render(
   <React.StrictMode>
-    <ThemeProvider>
-      <App />
-    </ThemeProvider>
+    <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
+
