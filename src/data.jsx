649f81a5aa630253b36ef7930eb2a4f923a0ed70
Ramy-Mansy <rami_manse@outlook.com>
2022-04-27 04:57:49 +0200

    add new project

--- src/data.jsx
+++ src/data.jsx
@@ -1,51 +1,56 @@
 export const products = [
   {
     id: 1,
+    img: "https://i.ibb.co/NZTZ6W6/screencapture-ecommerce-market-sigma-vercel-app-2022-04-27-04-48-48.png",
+    link: "https://ecommerce-market-sigma.vercel.app/",
+  },
+  {
+    id: 2,
     img: "./img/react-movies.png",
     link: "https://ramymansy.github.io/react-movies/",
   },
   {
-    id: 2,
+    id: 3,
     img: "https://i.ibb.co/gz2MsTt/Travel.png",
     link: "https://ramymansy.github.io/travel-react-app/",
   },
   {
-    id: 3,
+    id: 4,
     img: "https://i.ibb.co/zHhJyMR/screencapture-ramymansy-github-io-fashion-e-commerce-react-2022-03-23-20-23-18.png",
     link: "https://ramymansy.github.io/fashion-e-commerce-react/",
   },
   {
-    id: 4,
+    id: 5,
     img: "https://i.ibb.co/Y0tnhKd/bondi.jpg",
     link: "https://ramymansy.github.io/Bondi/",
   },
   {
-    id: 5,
+    id: 6,
     img: "https://i.ibb.co/q543GdF/clipboard.jpg",
     link: "https://ramymansy.github.io/Clipboard/",
   },
   {
-    id: 6,
+    id: 7,
     img: "https://i.ibb.co/3sbQY6X/leon.jpg",
     link: "https://ramymansy.github.io/leon/",
   },
   {
-    id: 7,
+    id: 8,
     img: "https://i.ibb.co/y4FM1Nb/zero.jpg",
     link: "https://ramymansy.github.io/Zero/",
   },
   {
-    id: 8,
+    id: 9,
     img: "https://i.ibb.co/MRpXrb7/huddle.jpg",
     link: "https://ramymansy.github.io/Huddle/",
   },
   {
-    id: 9,
+    id: 10,
     img: "https://i.ibb.co/378bGkJ/kasper.jpg",
     link: "https://ramymansy.github.io/kasper/",
   },
   {
-    id: 10,
+    id: 11,
     img: "https://i.ibb.co/XXDP4xP/fylo.jpg",
     link: "https://ramymansy.github.io/fylo/",
   },
