be6795f8ef2a766ff1d0c31483e550172cbc5aef
Ramy-Mansy <rami_manse@outlook.com>
2022-05-03 15:22:07 +0200

    add animation to clients section

--- src/components/client/Client.jsx
+++ src/components/client/Client.jsx
@@ -1,22 +1,33 @@
 import React from "react";
 import "./client.css";
+import Tilt from "react-vanilla-tilt";
 
 const Client = ({ img, quoteContent, quoteAuthor, jobTitle }) => {
   return (
-    <div className="cl">
-      <div className="author">
-        <div className="author-img">
-          <img src={img} alt="img" />
+    <Tilt
+      className="tilt"
+      options={{
+        max: 25,
+        speed: 1200,
+        glare: true,
+        "max-galre": 1,
+      }}
+    >
+      <div className="cl">
+        <div className="author">
+          <div className="author-img">
+            <img src={img} alt="img" />
+          </div>
+          <div className="author-title">
+            <h4 className="name">{quoteAuthor}</h4>
+            <h5 className="job">{jobTitle}</h5>
+          </div>
         </div>
-        <div className="author-title">
-          <h4 className="name">{quoteAuthor}</h4>
-          <h5 className="job">{jobTitle}</h5>
+        <div className="author-content">
+          <p className="content">{quoteContent}</p>
         </div>
       </div>
-      <div className="author-content">
-        <p className="content">{quoteContent}</p>
-      </div>
-    </div>
+    </Tilt>
   );
 };
 
